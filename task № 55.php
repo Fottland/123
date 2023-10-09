9992f879a938578b07f2415f10f2002d995ed88f
Fottland <146575211+Fottland@users.noreply.github.com>
2023-10-09 09:26:35 +0300

    Update task № 55.php
    Задание №55

--- task № 55.php
+++ task № 55.php
@@ -1,12 +1,4 @@
 <?php
-function normalizeUrl($add) {
-    $https = 'https://';
-    if ($add === $https) {
-    return $add;
-    }
-    else {
-     "https://{$add}";
-    }
- 
- }
- print_r(normalizeUrl('hello'));
+function flipFlop($action){
+    return $action == 'flip'? 'flop':'flip';
+}
